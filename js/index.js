714c8ea727eb2d9a3e733f1f5d1d3121f146dfd8
Edgar Ruiz <edgarruiz@Edgars-Air.lan>
2023-01-30 15:15:45 -0500

    asigment 6.1 AJA (XMLHttpRequest Object)

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 const today = new Date();
 
 const thisYear = today.getFullYear();
@@ -10,14 +11,15 @@ copyright.innerText = "Edgar Ruiz" + " " + thisYear;
 
 footer_parent.appendChild(copyright);
 
+/*
 const skills = [
     "JavaScript / HTML / CSS / Python / Git / GitHub / DOM", 
     "Microcomputer / Microship, Basic, Assembly", 
-    "Electronic circuit design / Altium Design", 
-    "PCB layout design / Altium Design", 
+    "Electronic circuit design / Altium Designer", 
+    "PCB layout design / Altium Designer", 
     "3D mechanical modeling / Creo PTC, SolidWorks, AutoCAD", 
     "Mechanical drawing standard / ASME Y14.5M", 
-    "PLM tools and engineering document control / Enovia", 
+    "PLM engineering document control / Enovia", 
 ];
 
 const skillsSection = document.getElementById("skills");
@@ -29,6 +31,8 @@ for( let x in skills) {
     skills_li.innerText = skills[x];
     skillsList.appendChild(skills_li);
 }
+*/
+
 
 const messageForm = document.querySelector("form");
 
